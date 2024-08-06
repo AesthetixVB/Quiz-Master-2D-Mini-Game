d8cf790c60c8918d435f566f2a377842ed01b316
TrezhaVB <vonbargen.trezha@gmail.com>
2024-08-05 20:39:04 -0700

    0.0.4
    - Quiz and QuestionsSO scripts are included
    - Text for Questions and Answers are synced.
        > However, button functionality has yet to be added.

--- Quiz Master/Assets/Scripts/QuestionSO.cs
+++ Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -7,7 +7,39 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string question = "Enter new question text here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
+
+
+    public string GetQuestion()
+    {
+        return question;
+
+    }
     
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+
+    }
+
+
 
 
 }
+
+//public class Test
+//{
+//    QuestionSO questionSO;
+//
+//    void TestA()
+//    {
+//        string questionText = questionSO.GetQuestion();
+//    }
+//}
